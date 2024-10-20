d7a8e07be79924178dd5d3669a00f71b7948157d
Caio Andrei <143545897+caioandrei@users.noreply.github.com>
2024-10-20 20:08:49 -0300

    Added MySQL JDBC driver to the project.

--- src/main/java/com/jdbc/App.java
+++ src/main/java/com/jdbc/App.java
@@ -1,13 +1,8 @@
 package com.jdbc;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
     }
 }
